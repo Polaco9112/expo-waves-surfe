166ab327592ffbb63000d486b655b69e66e3279a
cc-cesar-tobon <cesar.tobon@finoservices.com>
2024-09-06 11:35:32 -0500

    cambios

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet } from 'react-native';
-// import Waves from './src/components/Waves'
 import Waves from './src/jsx/Waves';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      {/* <Waves /> */}
       <Waves />
     </SafeAreaView>
   );
